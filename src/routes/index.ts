a7a56927c4e52bb736abce336f30f7fc6fdd9c0f
mhmdhalawi <mohammadhalawi93@gmail.com>
2025-08-13 19:16:33 +0200

    fix: update router history and Vite base path for production environment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+const base = import.meta.env.MODE === "production" ? "/roadsurfer/" : "/";
+
 const routes = [
   { path: "/", component: () => import("@/pages/Home.vue") },
   { path: "/about", component: () => import("@/pages/About.vue") },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(base),
   routes,
 });
 
